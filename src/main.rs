e719516930f85ac3958757f1ec81df4ffc6c39ed
David Weis <dweis7@gmail.com>
2024-04-18 02:32:06 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,133 @@
+use bevy::{
+    core::FrameCount,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{CursorGrabMode, PresentMode, WindowLevel, WindowTheme},
+};
+use bevy_prototype_lyon::prelude::*;
+use std::f64::consts::PI;
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa::Sample4)
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "robot face".into(),
+                    name: Some("face.app".into()),
+                    resolution: (480., 800.).into(),
+                    present_mode: PresentMode::AutoVsync,
+                    window_theme: Some(WindowTheme::Dark),
+                    enabled_buttons: bevy::window::EnabledButtons {
+                        maximize: false,
+                        minimize: false,
+                        ..Default::default()
+                    },
+                    visible: false,
+                    window_level: WindowLevel::AlwaysOnTop,
+                    mode: bevy::window::WindowMode::Fullscreen,
+                    cursor: bevy::window::Cursor {
+                        visible: false,
+                        grab_mode: CursorGrabMode::Confined,
+                        ..default()
+                    },
+
+                    ..default()
+                }),
+                ..default()
+            }),
+            LogDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+        ))
+        .add_plugins(ShapePlugin)
+        .add_systems(Startup, setup_system)
+        .add_systems(
+            Update,
+            (
+                toggle_fullscreen,
+                change_draw_mode_system,
+                change_number_of_sides,
+                rotate_shape_system,
+                make_visible,
+                bevy::window::close_on_esc,
+            ),
+        )
+        .run();
+}
+
+#[derive(Component)]
+struct ExampleShape;
+
+fn setup_system(mut commands: Commands) {
+    let shape = shapes::RegularPolygon {
+        sides: 6,
+        feature: shapes::RegularPolygonFeature::Radius(200.0),
+        ..shapes::RegularPolygon::default()
+    };
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        ShapeBundle {
+            path: GeometryBuilder::build_as(&shape),
+            ..default()
+        },
+        Fill::color(Color::CYAN),
+        Stroke::new(Color::BLACK, 10.0),
+        ExampleShape,
+    ));
+}
+
+fn rotate_shape_system(mut query: Query<&mut Transform, With<ExampleShape>>, time: Res<Time>) {
+    let delta = time.delta_seconds();
+
+    for mut transform in query.iter_mut() {
+        transform.rotate(Quat::from_rotation_z(0.2 * delta));
+    }
+}
+
+fn change_draw_mode_system(mut query: Query<(&mut Fill, &mut Stroke)>, time: Res<Time>) {
+    let hue = (time.elapsed_seconds_f64() * 50.0) % 360.0;
+    let outline_width = 2.0 + time.elapsed_seconds_f64().sin().abs() * 10.0;
+
+    for (mut fill_mode, mut stroke_mode) in query.iter_mut() {
+        fill_mode.color = Color::hsl(hue as f32, 1.0, 0.5);
+        stroke_mode.options.line_width = outline_width as f32;
+    }
+}
+
+fn change_number_of_sides(mut query: Query<&mut Path>, time: Res<Time>) {
+    let sides = ((time.elapsed_seconds_f64() - PI * 2.5).sin() * 2.5 + 5.5).round() as usize;
+
+    for mut path in query.iter_mut() {
+        let polygon = shapes::RegularPolygon {
+            sides,
+            feature: shapes::RegularPolygonFeature::Radius(200.0),
+            ..shapes::RegularPolygon::default()
+        };
+
+        *path = ShapePath::build_as(&polygon);
+    }
+}
+
+fn make_visible(mut window: Query<&mut Window>, frames: Res<FrameCount>) {
+    // The delay may be different for your app or system.
+    if frames.0 == 3 {
+        // At this point the gpu is ready to show the app so we can make the window visible.
+        // Alternatively, you could toggle the visibility in Startup.
+        // It will work, but it will have one white frame before it starts rendering
+        window.single_mut().visible = true;
+    }
+}
+
+// This system will toggle the color theme used by the window
+fn toggle_fullscreen(mut windows: Query<&mut Window>, input: Res<ButtonInput<KeyCode>>) {
+    if input.just_pressed(KeyCode::KeyF) {
+        let mut window = windows.single_mut();
+
+        window.mode = match window.mode {
+            bevy::window::WindowMode::Fullscreen => bevy::window::WindowMode::Windowed,
+            bevy::window::WindowMode::Windowed => bevy::window::WindowMode::Fullscreen,
+            _ => bevy::window::WindowMode::Windowed,
+        };
+    }
+}
